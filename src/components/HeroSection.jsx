ecf53e92dc3075c5b85e5c63b42e5fd23e1ed229
Nisarg Vadgama <nisargvv@gmail.com>
2021-07-05 16:20:10 -0400

    Add hero-section CSS file

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,8 @@
+import '../styles/hero-section.css';
+
 export default function HeroSection(props) {
   return(
-    <div>
+    <div className="hero">
       <h2>MAKING A BIG ORDER? <br/> WE GOT YOU.</h2>
       <p>Choose your quantity below to access our wholesale pricing. Wholesale orders are placed on demand and not recurring.</p>
     </div>
