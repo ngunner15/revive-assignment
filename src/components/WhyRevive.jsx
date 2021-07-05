7ef5692d95ac88cf9c483a5a64b1bfc40f5230c7
Nisarg Vadgama <nisargvv@gmail.com>
2021-07-05 19:18:31 -0400

    Create blocks and minor additions

--- src/components/WhyRevive.jsx
+++ src/components/WhyRevive.jsx
@@ -1,20 +1,36 @@
+import '../styles/why-revive.css';
+
 export default function WhyRevive(props) {
-  return(
-    <div>
-      <h2>WHY REVIVE</h2>
-      <p>Our goal is to help simplify your busy lifestyle with energizing, affordable, pre-portioned superfood smoothies and deliciously prepared meals. Simply select a plan, choose your superfood picks, and have it delivered frozen right to your door. Best of all, shipping is always FREE!</p>
-      <h2>FIND US AT THESE <br/> SELECT RETAILERS</h2>
-      <img src="pusateris.png" alt="Pusateris" />
-      <img src="pusateris.png" alt="Pusateris" />
-      <img src="pusateris.png" alt="Pusateris" />
-      <img src="logo/clock.png" alt="Clock" />
-      <h3>MEALS READY IN MINUTES</h3>
-      <img src="logo/chef's-hat.png" alt="Chef's Hat" />
-      <h3>CHEF CRAFTED</h3>
-      <img src="logo/hand.png" alt="Hand" />
-      <h3>PRESERVATIVE-FREE</h3>
-      <img src="logo/truck.png" alt="Truck" />
-      <h3>ALWAYS FREE SHIPPING</h3>
+  return (
+    <div className="why-revive">
+      <div className="why-revive-title">
+        <h3>WHY REVIVE</h3>
+        <p>Our goal is to help simplify your busy lifestyle with energizing, affordable, pre-portioned superfood smoothies and deliciously prepared meals. Simply select a plan, choose your superfood picks, and have it delivered frozen right to your door. Best of all, shipping is always FREE!</p>
+      </div>
+      <div className="pusateris">
+        <h3 className="find-us">FIND US AT THESE <br /> SELECT RETAILERS</h3>
+        <img src="pusateris.png" alt="Pusateris" />
+        <img src="pusateris.png" alt="Pusateris" />
+        <img src="pusateris.png" alt="Pusateris" />
+      </div>
+      <div className="information">
+        <div className="info-block">
+          <img src="logo/clock.png" alt="Clock" />
+          <h4>MEALS READY IN MINUTES</h4>
+        </div>
+        <div className="info-block">
+          <img src="logo/chef's-hat.png" alt="Chef's Hat" />
+          <h4>CHEF CRAFTED</h4>
+        </div>
+        <div className="info-block">
+          <img src="logo/hand.png" alt="Hand" />
+          <h4>PRESERVATIVE-FREE</h4>
+        </div>
+        <div className="info-block">
+          <img src="logo/truck.png" alt="Truck" />
+          <h4>ALWAYS FREE SHIPPING</h4>
+        </div>
+      </div>
     </div>
   );
 }
