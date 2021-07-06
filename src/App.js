d3d80a7f77314e8b37cff1ebc7a1d09bc785a2b5
Nisarg Vadgama <nisargvv@gmail.com>
2021-07-06 10:18:56 -0400

    Make Home component and add react routing

--- src/App.js
+++ src/App.js
@@ -3,9 +3,27 @@ import Header from './components/Header';
 import Selection from './components/Selection';
 import WhyRevive from './components/WhyRevive';
 import Footer from './components/Footer';
+import Main from './components/checkout/Main';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './styles/App.css';
 
 function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/checkout'>
+          <Main />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+function Home() {
   return (
     <div className="App">
       <Header />
