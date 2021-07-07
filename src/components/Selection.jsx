235b26fc6456299ba86ba50b8d54fd0a0d2e6f4c
Nisarg Vadgama <nisargvv@gmail.com>
2021-07-07 13:35:55 -0400

    Add Items component and onClick functionallity

--- src/components/Selection.jsx
+++ src/components/Selection.jsx
@@ -4,6 +4,7 @@ import Number from './Number';
 import Select from 'react-select';
 import '../styles/selection.css';
 
+// values for dropdown menu
 const options = [
   { label: '48', value: '48' },
   { label: '72', value: '72' },
@@ -17,6 +18,10 @@ export default function Selection(props) {
 
   const [selectedOption, setSelectedOption] = useState();
 
+  // logic to show Items onClick
+  const [showItems, setShowItems] = useState(false)
+  const onClick = () => setShowItems(true)
+
   const pricePerCup = 5.49;
 
   const handleChange = selectedOption => {
@@ -24,6 +29,7 @@ export default function Selection(props) {
     console.log(`Option selected:`, selectedOption);
   };
 
+  // send price and number of cups to Checkout Page
   let history = useHistory();
   const redirect = () => {
     history.push({
@@ -61,12 +67,75 @@ export default function Selection(props) {
         <h3>CHOOSE YOUR ITEMS</h3>
       </div>
       <div className="selection-box">
-        <img src="box/rect1.png" alt="fruity box" />
-        <img src="box/rect2.png" alt="best seller box" />
-        <img src="box/rect3.png" alt="fit box" />
+        <div className="selection-block">
+          <img src="box/rect1.png" alt="fruity box" onClick={onClick} />
+          <h5>FRUITY BOX</h5>
+          <p>Perfect for the entire family</p>
+        </div>
+        <div className="selection-block">
+          <img src="box/rect2.png" alt="best seller box" />
+          <h5>BEST SELLER BOX</h5>
+          <p>Our superfoodies’ favorites</p>
+        </div>
+        <div className="selection-block">
+          <img src="box/rect3.png" alt="fit box" />
+          <h5>FIT BOX</h5>
+          <p>Our superfoodies’ favorites</p>
+        </div>
       </div>
+      {showItems ? <Items /> : null}
       <button className="selection-button continue-button" onClick={redirect}><strong>Continue</strong></button>
-      <p className="selection-email">For bulk orders over 288 cups, please email:<br/> <u>wholesale@revivesuperfoods.com</u></p>
+      <p className="selection-email">For bulk orders over 288 cups, please email:<br /> <u>wholesale@revivesuperfoods.com</u></p>
     </div>
   );
 }
+
+// Items to show onClick
+const Items = () => (
+  <div className="items">
+    <h4>ITEMS IN THE FAMILY BOX</h4>
+    <div className="image-group">
+      <img src="pinkdragongroup.png" alt="pink dragon images" />
+      <img src="pinkdragongroup.png" alt="pink dragon images" />
+      <img src="pinkdragongroup.png" alt="pink dragon images" />
+    </div>
+    <div className="image-group-lp">
+      <div className="image-group-alt">
+        <img src="pinkdragon.png" alt="pink dragon" />
+        <p>pink dragon</p>
+      </div>
+      <div className="image-group-alt">
+        <img src="hintofmint.png" alt="hint of mint" />
+        <p>hint of mint</p>
+      </div>
+      <div className="image-group-alt">
+        <img src="pinkdragon.png" alt="pink dragon" />
+        <p>pink dragon</p>
+      </div>
+      <div className="image-group-alt">
+        <img src="hintofmint.png" alt="hint of mint" />
+        <p>hint of mint</p>
+      </div>
+      <div className="image-group-alt">
+        <img src="pinkdragon.png" alt="pink dragon" />
+        <p>pink dragon</p>
+      </div>
+      <div className="image-group-alt">
+        <img src="hintofmint.png" alt="hint of mint" />
+        <p>hint of mint</p>
+      </div>
+      <div className="image-group-alt">
+        <img src="pinkdragon.png" alt="pink dragon" />
+        <p>pink dragon</p>
+      </div>
+      <div className="image-group-alt">
+        <img src="hintofmint.png" alt="hint of mint" />
+        <p>hint of mint</p>
+      </div>
+      <div className="image-group-alt">
+        <img src="pinkdragon.png" alt="pink dragon" />
+        <p>pink dragon</p>
+      </div>
+    </div>
+  </div>
+)
