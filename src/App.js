c1fe1f0167f2de170c27cd830939e362cde62242
Nisarg Vadgama <nisargvv@gmail.com>
2021-07-06 23:10:47 -0400

    Add Info component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import HeroSection from './components/HeroSection';
 import Header from './components/Header';
 import Selection from './components/Selection';
 import WhyRevive from './components/WhyRevive';
+import Info from './components/Info';
 import Footer from './components/Footer';
 import Checkout from './components/checkout/Checkout';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -30,6 +31,7 @@ function Home() {
       <HeroSection />
       <Selection />
       <WhyRevive />
+      <Info />
       <Footer />
     </div>
   );
